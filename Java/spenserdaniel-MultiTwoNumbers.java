0a901e9469f0c0cf2c27b9a088775468f8935e37
Spenser Daniel <52738464+spenserdaniel@users.noreply.github.com>
2020-10-25 14:28:11 +0530

    Create spenserdaniel-MultiTwoNumbers.java

---
+++ Java/spenserdaniel-MultiTwoNumbers.java
@@ -0,0 +1,15 @@
+// PROGRAM-NAME : MultiplyTwoNumbers
+// By Spenser Daniel
+// PROGRAM-CODE :
+
+class product
+{
+public void main()
+{
+int a=9;
+int b=3;
+int prod=a*b;
+System.out.println("The Product of 2 Numbers="+prod);
+}
+}
+
