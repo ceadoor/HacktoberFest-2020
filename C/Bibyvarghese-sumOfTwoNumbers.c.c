3a38523458e0e647fe587358b2113a60a2dc17e3
Biby Varghese <56380834+bibyvarghese@users.noreply.github.com>
2020-10-25 13:28:25 +0530

    Create bibyvarghese-sumOfTwoNumbers.c

---
+++ C/Bibyvarghese-sumOfTwoNumbers.c.c
@@ -0,0 +1,14 @@
+// PROGRAM-NAME : Sum of two numbers
+// By biby varghese
+// PROGRAM-CODE :
+#include<stdio.h>
+int main()
+{
+int a, b, sum;
+printf("print a:");
+scanf(%d,&a);
+prinf("print b:");
+sum= a+b;
+printf(sum: %d,sum);
+return0;
+}
