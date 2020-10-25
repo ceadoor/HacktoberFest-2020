1f6f476622d26d84b720d30bd6353fafd384bfed
Tobin Jacob <51044536+tobinjacob@users.noreply.github.com>
2020-10-25 13:59:23 +0530

    Created tobinjacob_RollADice.py

---
+++ Python/tobinjacob_RollADice.py
@@ -0,0 +1,10 @@
+# PROGRAM-NAME : Roll A Dice
+# By Tobin Jacob
+# PROGRAM-CODE :
+
+import random
+
+while (1):
+	value=random.randrange(1,7)
+	print("\n",value)
+	input("\n\nRoll the Dice again? \nYes	1\nNo	Ctrl+C\n")
