014a2c73581e94fc739bd2676ae16d10350be134
Spenser Daniel <52738464+spenserdaniel@users.noreply.github.com>
2020-10-25 13:45:34 +0530

    Create spenserdaniel-MultiTwoNumbers.java

---
+++ Java/spenserdaniel-MultiTwoNumbers.java
@@ -0,0 +1,14 @@
+// PROGRAM-NAME : Multiply two Numbers
+// By Spenser Daniel
+// PROGRAM-CODE :
+
+class multitwo
+{
+public void main()
+{
+int a=5;
+int b=13;
+int prod=a*b;
+System.out.println("Product of two numbers="+prod);
+}
+}
