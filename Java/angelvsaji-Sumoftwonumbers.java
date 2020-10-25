4973a01c07ebc9c2a591282fa3bb7b0331f27489
ANGEL V SAJI <54198529+angelvsaji@users.noreply.github.com>
2020-10-25 00:17:57 -0700

    Create angelvsaji-Sumoftwonumbers.java

---
+++ Java/angelvsaji-Sumoftwonumbers.java
@@ -0,0 +1,16 @@
+//PROGRAM-NAME : Sum of Two Numbers
+// By 'Angel V Saji'
+// PROGRAM-CODE ;
+public class AddTwoNumbers {
+
+   public static void main(String[] args) {
+        
+      int num1 = 5, num2 = 15, sum;
+      sum = num1 + num2;
+
+      System.out.println("Sum of these numbers: "+sum);
+   }
+}
+
+
+
