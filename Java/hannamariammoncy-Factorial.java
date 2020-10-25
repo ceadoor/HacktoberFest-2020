963d8b22a8e555c8de6802ef098622d093425ad9
hannamariammoncy <56380746+hannamariammoncy@users.noreply.github.com>
2020-10-25 13:39:54 +0530

    Create hannamariammoncy-Factorial.java

---
+++ Java/hannamariammoncy-Factorial.java
@@ -0,0 +1,18 @@
+// PROGRAM-NAME :Factorial.java
+// By Hanna Mariam Moncy
+// PROGRAM-CODE :
+
+public class Factorial {
+
+    public static void main(String[] args) {
+
+        int num = 10;
+        long factorial = 1;
+        for(int i = 1; i <= num; ++i)
+        {
+            // factorial = factorial * i;
+            factorial *= i;
+        }
+        System.out.printf("Factorial of %d = %d", num, factorial);
+    }
+}
