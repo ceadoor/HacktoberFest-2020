0c670836d33bc365aeda407c6e803e4642afa875
sruthisgeorge <43113288+sruthisgeorge@users.noreply.github.com>
2020-10-25 15:36:05 +0530

    Create sruthishelloworld.c
    Created a file named hello wolrd

---
+++ C/sruthishelloworld.c
@@ -0,0 +1,9 @@
+// PROGRAM-NAME : Hello, World!
+// By Sruthi S
+// PROGRAM-CODE :
+#include <stdio.h>
+int main() {
+   // printf() displays the string inside quotation
+   printf("Hello, World!");
+   return 0;
+}
