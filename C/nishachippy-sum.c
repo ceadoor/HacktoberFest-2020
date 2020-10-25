8b4f5edcbb4864cb76c0ff383c182ef4e0da0b04
Nisha b <48785040+Nishachippy@users.noreply.github.com>
2020-10-25 13:19:45 +0530

    Create nishachippy-sum.c

---
+++ C/nishachippy-sum.c
@@ -0,0 +1,14 @@
+// PROGRAM-NAME : sum of 2 number
+// By John Doe
+// PROGRAM-CODE :
+#include<stdio.h>
+#include<stdio.h>
+int main(void)
+{
+int a,b,sum;
+printf("enter 2 number");
+scanf("%d%d",&a,&b);
+sum = a+b;
+printf("%d",sum);
+return Exit_success;
+}
