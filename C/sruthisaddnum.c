7f40adf6ff1794d6226ab5534ac29ac89208eb95
sruthisgeorge <43113288+sruthisgeorge@users.noreply.github.com>
2020-10-25 21:43:41 +0530

    Create sruthisaddnum.c
    Created add num.c

---
+++ C/sruthisaddnum.c
@@ -0,0 +1,16 @@
+//PROGRAM NAME:Add num
+//By sruthi s
+//PROGRAM CODE:#include <stdio.h>
+int main() {    
+
+    int number1, number2, sum;
+    
+    printf("Enter two integers: ");
+    scanf("%d %d", &number1, &number2);
+
+    // calculating sum
+    sum = number1 + number2;      
+    
+    printf("%d + %d = %d", number1, number2, sum);
+    return 0;
+}
