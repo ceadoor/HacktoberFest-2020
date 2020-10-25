b08f26606cc2c60af3d759f4e5a92ea9c4bdd639
iswarya-123 <56380766+iswarya-123@users.noreply.github.com>
2020-10-25 20:48:25 +0530

    create iswarya-123-Sum.py

---
+++ Python/iswarya-123-Sum.py
@@ -0,0 +1,11 @@
+# PROGRAM-NAME : Sum Of Two Numbers
+# By S Iswarya
+# PROGRAM-CODE :
+num1 = input('Enter first number: ')
+num2 = input('Enter second number: ')
+
+# Add two numbers
+sum = float(num1) + float(num2)
+
+# Display the sum
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
