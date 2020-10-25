9efe70665b677c2707f56a2e983d6a407cff25d6
Nisha b <48785040+Nishachippy@users.noreply.github.com>
2020-10-25 13:39:55 +0530

    Create chippy-helloworld.java

---
+++ Java/chippy-helloworld.java
@@ -0,0 +1,8 @@
+// PROGRAM-NAME : Hello, World!
+// By John Doe
+// PROGRAM-CODE :
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+    }
+}
