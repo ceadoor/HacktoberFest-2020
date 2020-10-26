fe0e5805d5d1573f95e58e74f05a4289ece5ced9
Gejoprajan <73432088+Gejoprajan@users.noreply.github.com>
2020-10-26 21:07:35 +0530

    Create gejoprajan-sumoftwonos.c

---
+++ C/gejoprajan-sumoftwonos.c
@@ -0,0 +1,17 @@
+// PROGRAM-NAME : Sum of two numbers!
+// By Gejo Rajan
+// PROGRAM-CODE :
+#include <stdio.h>
+int main() {    
+
+    int number1, number2, sum;
+    
+    printf("Enter two integers: ");
+    scanf("%d %d", &number1, &number2);
+
+    // calculating sum
+    sum = number1 + number2;      
+    
+    printf("%d + %d = %d", number1, number2, sum);
+    return 0;
+}
