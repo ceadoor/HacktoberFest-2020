b6cd9ffe13b881121704284c6fc3dee853aa9d88
sruthisgeorge <43113288+sruthisgeorge@users.noreply.github.com>
2020-10-27 01:43:59 +0530

    added delete an element

---
+++ Other/SruthiS-DeleteElement.php
@@ -0,0 +1,23 @@
+// PROGRAM-NAME : Delete Element from an array !
+// By Sruthi S George 
+//PROGRAM-CODE :
+
+<?php 
+// PHP program to delete an array  
+// element based on index 
+  
+// Declare arr variable 
+// and initialize it 
+$arr = array('G', 'E', 'E', 'K', 'S'); 
+  
+// Display the array element 
+print_r($arr); 
+  
+// Use unset() function delete 
+// elements 
+unset($arr[2]); 
+  
+// Display the array element 
+print_r($arr); 
+  
+?> 
