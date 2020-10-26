b11f8e6cfa9229a4eb025fe1fcbfe921e19d7b53
basimca <56380539+basimca@users.noreply.github.com>
2020-10-26 09:45:17 +0530

    Create basimca-AddTwoIntegers.cpp

---
+++ C++/basimca-AddTwoIntegers.cpp
@@ -0,0 +1,21 @@
+// PROGRAM-NAME :  Add Two Add Two Integers
+// By Ahammed Basim
+// PROGRAM-CODE :
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int firstNumber, secondNumber, sumOfTwoNumbers;
+    
+    cout << "Enter two integers: ";
+    cin >> firstNumber >> secondNumber;
+
+    // sum of two numbers in stored in variable sumOfTwoNumbers
+    sumOfTwoNumbers = firstNumber + secondNumber;
+
+    // Prints sum 
+    cout << firstNumber << " + " <<  secondNumber << " = " << sumOfTwoNumbers;     
+
+    return 0;
+}
