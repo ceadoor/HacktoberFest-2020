fd33bb97761c85e016635a9468f69d8153e5e9e3
NavneethChandran <44723640+NavneethChandran@users.noreply.github.com>
2020-10-27 07:03:18 +0530

    Add NavneethChandran-CopyString.cpp

---
+++ C++/NavneethChandran-CopyString.cpp
@@ -0,0 +1,21 @@
+// PROGRAM-NAME : Copy Strings
+// By Navneeth Chandran
+// PROGRAM-CODE :
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string s1, s2;
+
+    cout << "Enter string s1: ";
+    getline (cin, s1);
+
+    s2 = s1;
+
+    cout << "s1 = "<< s1 << endl;
+    cout << "s2 = "<< s2;
+
+    return 0;
+}
