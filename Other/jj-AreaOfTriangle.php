f5de6ff51f0022cbf67bb6af2c65f3a4f56a9678
JeromJosephInTech <73548038+JeromJosephInTech@users.noreply.github.com>
2020-10-27 22:21:06 +0530

    Create jj-AreaOfTriangle.php

---
+++ Other/jj-AreaOfTriangle.php
@@ -0,0 +1,10 @@
+// PROGRAM-NAME : Area Of Triangle !
+// By John Doe
+
+//PROGRAM-CODE :
+    
+    <?php  
+     $base = 10;  
+     $height = 15;  
+     echo "area with base $base and height $height= " . ($base * $height) / 2;  
+     ?>  
