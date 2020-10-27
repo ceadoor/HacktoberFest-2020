c1013ddb93142c686c41221c53018163b8b65b18
JeromJosephInTech <73548038+JeromJosephInTech@users.noreply.github.com>
2020-10-27 22:24:34 +0530

    add Diamond Like Program

---
+++ Other/StarDiamondPrgm.php
@@ -0,0 +1,25 @@
+// PROGRAM-NAME : Diamond Like Program !
+// By Jerome Joseph
+
+//PROGRAM-CODE :
+   
+   <?php  
+    for($i=0;$i<=5;$i++){  
+    for($k=5;$k>=$i;$k--){  
+    echo "  ";  
+    }  
+    for($j=1;$j<=$i;$j++){  
+    echo "*  ";  
+    }  
+    echo "<br>";  
+    }  
+    for($i=4;$i>=1;$i--){  
+    for($k=5;$k>=$i;$k--){  
+    echo "  ";  
+    }  
+    for($j=1;$j<=$i;$j++){  
+    echo "*  ";  
+    }  
+    echo "<br>";  
+    }  
+    ?>  
