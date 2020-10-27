bb6ed31ccd4e013d57f5971a3fa5515d7d87d369
JeromJosephInTech <73548038+JeromJosephInTech@users.noreply.github.com>
2020-10-27 22:18:08 +0530

    Create TableofNumbers.php

---
+++ Other/TableofNumbers.php
@@ -0,0 +1,13 @@
+// PROGRAM-NAME : Table of Numbers !
+// By Jerome Joseph
+
+//PROGRAM-CODE :   
+
+   <?php    
+    define('a', 7);   
+    for($i=1; $i<=10; $i++)   
+    {   
+      echo $i*a;   
+      echo '<br>';     
+    }  
+    ?>  
